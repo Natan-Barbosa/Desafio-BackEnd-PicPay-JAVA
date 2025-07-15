d69f87bfaca4e0d9493a04b4194a9cf91751cb23
natan <natanbarbosax193@gmail.com>
2025-07-15 20:05:26 -0300

    edit: add manual builder to validate CPF and CNPJ

--- src/main/java/com/barbosa/desafiobackendpicpay/Entities/WalletEntity.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Entities/WalletEntity.java
@@ -1,5 +1,7 @@
 package com.barbosa.desafiobackendpicpay.Entities;
 
+import com.barbosa.desafiobackendpicpay.Exceptions.InvalidCNPJException;
+import com.barbosa.desafiobackendpicpay.Exceptions.InvalidCPFException;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,7 +12,6 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Getter
 @Setter
-@Builder
 @AllArgsConstructor
 public class WalletEntity {
 
@@ -36,4 +37,68 @@ public class WalletEntity {
     @Column(name = "wallet_type",nullable = false)
     @Enumerated(EnumType.STRING)
     private WalletTypeEnum walletType;
+
+    public static WalletEntityBuilder builder() {
+        return new WalletEntityBuilder();
+    }
+
+    public static class WalletEntityBuilder {
+        private String id;
+        private String fullName;
+        private String cpfOrcnpj;
+        private String email;
+        private BigDecimal balance = BigDecimal.ZERO;
+        private String password;
+        private WalletTypeEnum walletType;
+
+        public WalletEntityBuilder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public WalletEntityBuilder fullName(String fullName) {
+            this.fullName = fullName;
+            return this;
+        }
+
+        public WalletEntityBuilder cpfOrcnpj(String cpfOrcnpj) {
+            this.cpfOrcnpj = cpfOrcnpj;
+            return this;
+        }
+
+        public WalletEntityBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public WalletEntityBuilder balance(BigDecimal balance) {
+            this.balance = balance;
+            return this;
+        }
+
+        public WalletEntityBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public WalletEntityBuilder walletType(WalletTypeEnum walletType) {
+            this.walletType = walletType;
+            return this;
+        }
+
+        public WalletEntity build() {
+            // Validação antes de construir
+            validateCpfCnpj();
+            return new WalletEntity(id, fullName, cpfOrcnpj, email, balance, password, walletType);
+        }
+
+        private void validateCpfCnpj() {
+            if (walletType == WalletTypeEnum.USER && cpfOrcnpj.length() != 11) {
+                throw new InvalidCPFException("Invalid CPF, please Verify the sended Data");
+            }
+            if (walletType == WalletTypeEnum.SELLER && cpfOrcnpj.length() != 14) {
+                throw new InvalidCNPJException("Invalid CNPJ, please Verify the sended Data");
+            }
+        }
+    }
 }
