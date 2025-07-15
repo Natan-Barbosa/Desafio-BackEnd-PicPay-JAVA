23031244a4daa7999c89e9ba07b67ab4b525276d
natan <natanbarbosax193@gmail.com>
2025-07-15 20:04:43 -0300

    edit: add message to errors

--- src/main/java/com/barbosa/desafiobackendpicpay/Services/Create/WalletCreateDto.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Services/Create/WalletCreateDto.java
@@ -1,27 +1,33 @@
 package com.barbosa.desafiobackendpicpay.Services.Create;
 
 import com.barbosa.desafiobackendpicpay.Entities.WalletTypeEnum;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class WalletCreateDto {
 
-    @NotEmpty
+    @NotEmpty(message = "Field Cannot Be Empty")
     private String fullName;
 
-    @NotEmpty
+    @NotEmpty(message = "Field Cannot Be Empty")
     private String cpfOrcnpj;
 
-    @NotEmpty
+    @NotEmpty(message = "Field Cannot Be Empty")
+    @Email(message = "Invalid Email")
     private String email;
 
-    @NotEmpty
+    @NotEmpty(message = "Field Cannot Be Empty")
+    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
     private String password;
 
+    @NotNull(message = "Field Cannot Be Null")
     private WalletTypeEnum walletType;
 }
