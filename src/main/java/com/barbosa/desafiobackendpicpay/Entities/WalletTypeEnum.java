35b3fc7da7c0655ec64c9c9770543d581ea34950
natan <natanbarbosax193@gmail.com>
2025-07-15 18:26:31 -0300

    Add: Wallet type enum

---
+++ src/main/java/com/barbosa/desafiobackendpicpay/Entities/WalletTypeEnum.java
@@ -0,0 +1,14 @@
+package com.barbosa.desafiobackendpicpay.Entities;
+
+public enum WalletTypeEnum {
+    USER(1,"user"),
+    SELLER(2,"seller");
+
+    WalletTypeEnum(Integer id, String userType) {
+        this.id = id;
+        this.userType = userType;
+    }
+
+    private Integer id;
+    private String userType;
+}
