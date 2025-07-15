9a198ed89bbae4928660fbe80fbdacaa8e293486
natan <natanbarbosax193@gmail.com>
2025-07-15 20:05:59 -0300

    add: wallet controller

---
+++ src/main/java/com/barbosa/desafiobackendpicpay/Controller/WalletController.java
@@ -0,0 +1,29 @@
+package com.barbosa.desafiobackendpicpay.Controller;
+
+import com.barbosa.desafiobackendpicpay.Entities.WalletEntity;
+import com.barbosa.desafiobackendpicpay.Services.Create.WalletCreateDto;
+import com.barbosa.desafiobackendpicpay.Services.Create.WalletCreateService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wallet")
+public class WalletController {
+
+    @Autowired
+    WalletCreateService walletCreateService;
+
+
+    @PostMapping
+    public ResponseEntity<WalletEntity> create(@Valid @RequestBody WalletCreateDto dto) {
+        WalletEntity serviceResponse = walletCreateService.create(dto);
+        return new ResponseEntity<>(serviceResponse, HttpStatus.CREATED);
+    }
+
+}
