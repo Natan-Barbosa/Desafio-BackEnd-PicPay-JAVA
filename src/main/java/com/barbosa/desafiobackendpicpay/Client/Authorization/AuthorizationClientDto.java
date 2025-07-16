374be4528e950fa21463206e246bfa6f0ff07752
natan <natanbarbosax193@gmail.com>
2025-07-16 14:18:01 -0300

    add: dto to request in api

---
+++ src/main/java/com/barbosa/desafiobackendpicpay/Client/Authorization/AuthorizationClientDto.java
@@ -0,0 +1,8 @@
+package com.barbosa.desafiobackendpicpay.Client.Authorization;
+
+import java.util.Map;
+
+public class AuthorizationClientDto {
+    private String status;
+    private Map<String,Object> data;
+}
