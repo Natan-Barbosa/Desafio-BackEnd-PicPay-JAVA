8437d1b1e3a8bec983559d86539bab3052d34217
natan <natanbarbosax193@gmail.com>
2025-07-16 14:17:42 -0300

    edit: add feign annotation

--- src/main/java/com/barbosa/desafiobackendpicpay/DesafiobackendpicpayApplication.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/DesafiobackendpicpayApplication.java
@@ -2,12 +2,14 @@ package com.barbosa.desafiobackendpicpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DesafiobackendpicpayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafiobackendpicpayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesafiobackendpicpayApplication.class, args);
+    }
 
 }
