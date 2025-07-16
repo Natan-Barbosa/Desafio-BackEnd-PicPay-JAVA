a7bcbed7f1f215d66ef07b8ed1364a5db46bd155
natan <natanbarbosax193@gmail.com>
2025-07-16 14:18:40 -0300

    add: NotificationClient

---
+++ src/main/java/com/barbosa/desafiobackendpicpay/Client/Notification/NotificationClient.java
@@ -0,0 +1,13 @@
+package com.barbosa.desafiobackendpicpay.Client.Notification;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(url = "https://util.devi.tools/api/v1/notify",
+        fallback = NotificationFallback.class,
+        name = "notificationClient")
+public interface NotificationClient {
+
+    @PostMapping
+    void sendNotification();
+}
