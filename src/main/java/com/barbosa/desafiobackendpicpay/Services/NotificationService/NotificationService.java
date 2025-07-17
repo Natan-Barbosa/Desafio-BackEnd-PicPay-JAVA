cc057c2e40ceba31db64ba6e706734f73be58fba
natan <natanbarbosax193@gmail.com>
2025-07-17 13:02:00 -0300

    edit: add exception catch

--- src/main/java/com/barbosa/desafiobackendpicpay/Services/NotificationService/NotificationService.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Services/NotificationService/NotificationService.java
@@ -1,16 +1,26 @@
 package com.barbosa.desafiobackendpicpay.Services.NotificationService;
 
 import com.barbosa.desafiobackendpicpay.Client.Notification.NotificationClient;
+import feign.FeignException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationService {
 
+    private static final Logger log = LogManager.getLogger(NotificationService.class);
     @Autowired
     private NotificationClient notificationClient;
 
-    public void sendNotification(){
-        notificationClient.sendNotification();
+    public void sendNotification() {
+        try {
+            notificationClient.sendNotification();
+        } catch (FeignException.GatewayTimeout e) {
+            log.error(e.getMessage());
+        } catch (FeignException e) {
+            log.error(e.getMessage());
+        }
     }
 }
