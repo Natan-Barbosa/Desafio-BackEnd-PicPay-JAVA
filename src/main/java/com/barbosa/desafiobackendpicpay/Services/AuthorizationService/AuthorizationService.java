1ae020e8b896cac0fa59aeccab947570ea5c7b68
natan <natanbarbosax193@gmail.com>
2025-07-17 12:59:38 -0300

    edit: add exception catch

--- src/main/java/com/barbosa/desafiobackendpicpay/Services/AuthorizationService/AuthorizationService.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Services/AuthorizationService/AuthorizationService.java
@@ -1,10 +1,9 @@
 package com.barbosa.desafiobackendpicpay.Services.AuthorizationService;
 
 import com.barbosa.desafiobackendpicpay.Client.Authorization.AuthorizationClient;
-import com.barbosa.desafiobackendpicpay.Client.Authorization.AuthorizationClientDto;
 import com.barbosa.desafiobackendpicpay.Exceptions.Transaction.AuthorizationException;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,9 +13,12 @@ public class AuthorizationService {
     private AuthorizationClient authorizationClient;
 
     public void authorize() {
-        ResponseEntity<AuthorizationClientDto> authorization = authorizationClient.authorization();
-        if (authorization.getStatusCode().is4xxClientError()) {
-            throw new AuthorizationException("Transaction Not Authorized");
+        try {
+            authorizationClient.authorization();
+        } catch (FeignException.Forbidden ex) {
+            throw new AuthorizationException("Transaction Not Authorized, You Can Try Later");
+        } catch (FeignException ex) {
+            throw new AuthorizationException("Transaction Not Authorized, You Can Try Later");
         }
     }
 
