001f5e557a4466b4ee3ad4a8684ad292e913817d
natan <natanbarbosax193@gmail.com>
2025-07-17 16:41:44 -0300

    edit: add transaction annotation

--- src/main/java/com/barbosa/desafiobackendpicpay/Services/WalletService/Create/WalletCreateService.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Services/WalletService/Create/WalletCreateService.java
@@ -5,10 +5,12 @@ import com.barbosa.desafiobackendpicpay.Exceptions.Wallet.WalletAlreadyExistsExc
 import com.barbosa.desafiobackendpicpay.Repository.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional
 public class WalletCreateService {
 
     @Autowired
