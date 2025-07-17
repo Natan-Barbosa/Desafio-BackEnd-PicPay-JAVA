6c12d0807b194c8a0c43567fa1a9378f69dbc57c
natan <natanbarbosax193@gmail.com>
2025-07-17 13:01:28 -0300

    edit: remove fallback

--- src/main/java/com/barbosa/desafiobackendpicpay/Client/Notification/NotificationClient.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Client/Notification/NotificationClient.java
@@ -3,9 +3,7 @@ package com.barbosa.desafiobackendpicpay.Client.Notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(url = "https://util.devi.tools/api/v1/notify",
-        fallback = NotificationFallback.class,
-        name = "notificationClient")
+@FeignClient(url = "https://util.devi.tools/api/v1/notify", name = "notificationClient")
 public interface NotificationClient {
 
     @PostMapping
