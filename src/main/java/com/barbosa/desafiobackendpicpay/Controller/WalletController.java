ccd387fe5a144fb8fe1c45272e8cdcd9fd74c629
natan <natanbarbosax193@gmail.com>
2025-07-17 13:03:23 -0300

    edit: add increase and get endpoints

--- src/main/java/com/barbosa/desafiobackendpicpay/Controller/WalletController.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Controller/WalletController.java
@@ -3,14 +3,16 @@ package com.barbosa.desafiobackendpicpay.Controller;
 import com.barbosa.desafiobackendpicpay.Entities.Wallet.WalletEntity;
 import com.barbosa.desafiobackendpicpay.Services.WalletService.Create.WalletCreateDto;
 import com.barbosa.desafiobackendpicpay.Services.WalletService.Create.WalletCreateService;
+import com.barbosa.desafiobackendpicpay.Services.WalletService.GetWalletInfo.GetWalletInfoDto;
+import com.barbosa.desafiobackendpicpay.Services.WalletService.GetWalletInfo.GetWalletInfoService;
+import com.barbosa.desafiobackendpicpay.Services.WalletService.IncreaseBalance.IncreaseBalanceDto;
+import com.barbosa.desafiobackendpicpay.Services.WalletService.IncreaseBalance.IncreaseBalanceService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wallet")
@@ -19,10 +21,27 @@ public class WalletController {
     @Autowired
     WalletCreateService walletCreateService;
 
+    @Autowired
+    IncreaseBalanceService increaseBalanceService;
 
-    @PostMapping
+    @Autowired
+    GetWalletInfoService getWalletInfoService;
+
+    @PostMapping("create")
     public ResponseEntity<WalletEntity> create(@Valid @RequestBody WalletCreateDto dto) {
         WalletEntity serviceResponse = walletCreateService.create(dto);
+        return new ResponseEntity<>(serviceResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<GetWalletInfoDto> get(@PathVariable @NotEmpty String id) {
+        GetWalletInfoDto serviceResponse = getWalletInfoService.get(id);
+        return new ResponseEntity<>(serviceResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("increase")
+    public ResponseEntity<String> increase(@Valid @RequestBody IncreaseBalanceDto dto) {
+        String serviceResponse = increaseBalanceService.increase(dto);
         return new ResponseEntity<>(serviceResponse, HttpStatus.CREATED);
     }
 
