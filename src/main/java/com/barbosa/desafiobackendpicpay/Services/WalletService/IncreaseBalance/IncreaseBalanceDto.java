243d1763b21606766ed35403b59ce3729c4ec7df
natan <natanbarbosax193@gmail.com>
2025-07-17 13:00:37 -0300

    add: IncreaseBalanceDto

---
+++ src/main/java/com/barbosa/desafiobackendpicpay/Services/WalletService/IncreaseBalance/IncreaseBalanceDto.java
@@ -0,0 +1,21 @@
+package com.barbosa.desafiobackendpicpay.Services.WalletService.IncreaseBalance;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class IncreaseBalanceDto {
+    @NotEmpty(message = "Field Cannot Be Empty")
+    private String cpfOrcnpj;
+
+    @NotEmpty(message = "Field Cannot Be Empty")
+    private String password;
+
+    @Min(value = 1, message = "The Minimum value is 1,Please insert a valid value")
+    private BigDecimal value;
+}
