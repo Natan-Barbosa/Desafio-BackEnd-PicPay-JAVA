a0b2a0e77f2f8b6e99939e48fcce41b369996acf
natan <natanbarbosax193@gmail.com>
2025-07-17 16:41:35 -0300

    edit: add builder

--- src/main/java/com/barbosa/desafiobackendpicpay/Services/WalletService/Create/WalletCreateDto.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Services/WalletService/Create/WalletCreateDto.java
@@ -5,13 +5,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class WalletCreateDto {
 
     @NotEmpty(message = "Field Cannot Be Empty")
