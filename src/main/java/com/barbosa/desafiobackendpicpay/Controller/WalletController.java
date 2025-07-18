42ad7e616ce8cbbf5d336a29901ae2d859ff8189
natan <natanbarbosax193@gmail.com>
2025-07-18 14:56:09 -0300

    edit: add validated to previne null attribute in variable path

--- src/main/java/com/barbosa/desafiobackendpicpay/Controller/WalletController.java
+++ src/main/java/com/barbosa/desafiobackendpicpay/Controller/WalletController.java
@@ -8,14 +8,18 @@ import com.barbosa.desafiobackendpicpay.Services.WalletService.GetWalletInfo.Get
 import com.barbosa.desafiobackendpicpay.Services.WalletService.IncreaseBalance.IncreaseBalanceDto;
 import com.barbosa.desafiobackendpicpay.Services.WalletService.IncreaseBalance.IncreaseBalanceService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wallet")
+@Validated
 public class WalletController {
 
     @Autowired
@@ -34,7 +38,7 @@ public class WalletController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<GetWalletInfoDto> get(@PathVariable @NotEmpty String id) {
+    public ResponseEntity<GetWalletInfoDto> get(@PathVariable @NotNull @NotEmpty String id) {
         GetWalletInfoDto serviceResponse = getWalletInfoService.get(id);
         return new ResponseEntity<>(serviceResponse, HttpStatus.OK);
     }
